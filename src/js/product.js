77ff168185d7e027d069dd37c4c076466c494945
Zureyma-Manrique <quetzaili.manrique@gmail.com>
2025-11-04 22:56:49 -0600

    W02 team

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,15 @@
 import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs'
+
+// get the product id from the URL
+const productId = getParam('product');
+// create a new ProductData object for 'tents'
+
+// create a new ProductDetails object
+const product = new ProductDetails(productId, dataSource);
+// initialize the product details page
+product.init();
 
 const dataSource = new ProductData('tents');
 
