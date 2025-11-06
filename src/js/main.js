009c2a9456857f95b1702a8682fc796bdc11bc6c
Zureyma-Manrique <quetzaili.manrique@gmail.com>
2025-11-05 20:47:50 -0600

    W02 individual

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { qs } from './utils.mjs';
+
+// Get the element to insert the product cards into
+const productListElement = qs('.product-list');
+
+// Create an instance of ProductData for 'tents'
+const dataSource = new ProductData('tents');
+
+// Create an instance of ProductList
+// Pass in the category, the data source, and the target element
+const productList = new ProductList('tents', dataSource, productListElement);
+
+// Initialize the product list
+productList.init();
